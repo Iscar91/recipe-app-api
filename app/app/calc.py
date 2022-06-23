dee5d38040ba4a640cd5dcdb9b320eb06d1580ea
Isaac Carbajal <isaac@Macmini.local>
2022-06-23 10:55:02 -0500

    Add postgres image to docker-compose

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+def add(z,y):
+    """
+    Add two numbers
+    """
+    return z + y
+
+def subtract(x,y):
+    """
+    Subtract two numbers
+    """
+    return y - x
